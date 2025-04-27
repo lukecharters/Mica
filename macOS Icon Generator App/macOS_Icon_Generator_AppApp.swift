26931d395848389af9917682ebd53ae58e505d68
lukecharters <5044583+lukecharters@users.noreply.github.com>
2025-04-27 13:36:55 +1000

    Initial Commit

---
+++ macOS Icon Generator App/macOS_Icon_Generator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOS_Icon_Generator_AppApp.swift
+//  macOS Icon Generator App
+//
+//  Created by Luke Charters on 27/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct macOS_Icon_Generator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ macOS Icon Generator AppUITests/macOS_Icon_Generator_AppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  macOS_Icon_Generator_AppUITestsLaunchTests.swift
+//  macOS Icon Generator AppUITests
+//
+//  Created by Luke Charters on 27/4/2025.
+//
+
+import XCTest
+
+final class macOS_Icon_Generator_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
